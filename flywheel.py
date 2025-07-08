1d199c3ad310605dc90da58c5847fa869d928d47
Joe Reddington <joe@joereddington.com>
2025-07-08 20:58:53 +0100

    Flywheel ordered

--- flywheel.py
+++ flywheel.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Wedge
 
-def draw_alternating_circle(n_segments=60, diameter_cm=29.0, margin_cm=0.0):
+def draw_alternating_circle(n_segments=60, diameter_cm=29.0):
     dpi = 300
-    fig_width_cm = 29.7
-    fig_height_cm = 42.0
+    fig_width_cm = 30.0
+    fig_height_cm = 30.0
     fig_size_inches = (fig_width_cm / 2.54, fig_height_cm / 2.54)
     radius = diameter_cm / 2.0
-    drawing_limit = radius  # 14 + 1 = 15cm in each direction
 
     fig, ax = plt.subplots(figsize=fig_size_inches, dpi=dpi)
     ax.set_aspect('equal')
+    ax.axis('off')  # Turn off axes
 
     for i in range(n_segments):
         theta1 = (360 / n_segments) * i
@@ -21,12 +21,11 @@ def draw_alternating_circle(n_segments=60, diameter_cm=29.0, margin_cm=0.0):
                       facecolor=color, edgecolor='gray')
         ax.add_patch(wedge)
 
-    # Add 1 cm margin around the circle
-    ax.set_xlim(-drawing_limit, drawing_limit)
-    ax.set_ylim(-drawing_limit, drawing_limit)
+    ax.set_xlim(-radius, radius)
+    ax.set_ylim(-radius, radius)
 
-    # Save to full A3 size without cropping
-    fig.savefig("design.png", dpi=dpi, bbox_inches=None, pad_inches=0)
+    # Save the figure exactly as 30cm x 30cm
+    fig.savefig("design.png", dpi=dpi, bbox_inches='tight', pad_inches=0)
     plt.close(fig)
 
 draw_alternating_circle()
